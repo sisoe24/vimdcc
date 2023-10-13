24969892517f96742605e0b0df6cc528feb8cec9
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-10-12 22:06:16 -0400

    refactor

--- nuke_vim_editor/handlers/handlers_normal.py
+++ nuke_vim_editor/handlers/handlers_normal.py
@@ -115,8 +115,14 @@ class DocumentHandler(BaseHandler):
 
 @register_normal_handler
 class SearchHandler(BaseHandler):
+    last_match = None
+
     def __init__(self, editor: QPlainTextEdit):
         super().__init__(editor)
+        self.last_search_char = None
+        self.last_search_direction = None
+        self.last_search_stop_before = None
+        self.last_word_under_cursor = None
 
     def _find_word_in_line(self, cursor: QTextCursor, document: QTextDocument, line_number: int, word: str):
         line = document.findBlockByLineNumber(line_number)
@@ -132,6 +138,7 @@ class SearchHandler(BaseHandler):
         cursor.movePosition(QTextCursor.EndOfWord, QTextCursor.KeepAnchor)
 
         word_under_cursor = cursor.selectedText()
+        self.last_word_under_cursor = word_under_cursor
         document = self.editor.document()
         current_line = cursor.blockNumber()
 
@@ -152,6 +159,60 @@ class SearchHandler(BaseHandler):
 
         return False
 
+    def move_cursor(self, key_sequence, cursor, direction='forward', stop_before=False):
+        if len(key_sequence) == 2:
+            target_char = key_sequence[1]
+            cursor_position = cursor.position()
+            block_position = cursor.block().position()
+
+            # Calculate the relative cursor position within the current line
+            relative_cursor_position = cursor_position - block_position
+
+            # Get the text of the current line
+            current_line = cursor.block().text()
+
+            if direction == 'forward':
+                # Only consider the part of the line after the cursor
+                line_to_search = current_line[relative_cursor_position + 1:]
+                find_result = line_to_search.find(target_char)
+            else:
+                # Only consider the part of the line before the cursor
+                line_to_search = current_line[:relative_cursor_position]
+                find_result = line_to_search.rfind(target_char)
+
+            if find_result == -1:
+                return False
+
+            # Calculate the new cursor position in the context of the entire document
+            if direction == 'forward':
+                new_cursor_position = block_position + relative_cursor_position + 1 + find_result
+            else:
+                new_cursor_position = block_position + find_result
+
+            if stop_before and direction == 'forward':
+                new_cursor_position -= 1
+            elif stop_before and direction == 'backward':
+                new_cursor_position += 1
+
+            self.last_search_char = target_char
+            self.last_search_direction = direction
+            self.last_search_stop_before = stop_before
+
+            cursor.setPosition(new_cursor_position)
+            return True
+        return False
+
+    def repeat_last_search(self, cursor, reverse=False):
+        if self.last_search_char is None:
+            return False  # No previous search to repeat
+
+        direction = self.last_search_direction
+        if reverse:
+            direction = 'backward' if direction == 'forward' else 'forward'
+
+        key_sequence = ' ' + self.last_search_char  # Placeholder for the first character
+        return self.move_cursor(key_sequence, cursor, direction, self.last_search_stop_before)
+
     def handle(self, cursor: QTextCursor, key_sequence: str, modifiers: List[str], event: QKeyEvent):
 
         if key_sequence == '#':
@@ -160,6 +221,30 @@ class SearchHandler(BaseHandler):
 
         if key_sequence == "*":
             self._find_word_in_document(cursor, "down")
+            return True
+
+        if key_sequence.startswith("f") and len(key_sequence) == 2:
+            return self.move_cursor(key_sequence, cursor, 'forward', stop_before=False)
+
+        if key_sequence.startswith("F") and len(key_sequence) == 2:
+            return self.move_cursor(key_sequence, cursor, 'backward', stop_before=False)
+
+        if key_sequence.startswith("t") and len(key_sequence) == 2:
+            return self.move_cursor(key_sequence, cursor, 'forward', stop_before=True)
+
+        if key_sequence.startswith("T") and len(key_sequence) == 2:
+            return self.move_cursor(key_sequence, cursor, 'backward', stop_before=True)
+
+        if key_sequence == ';':
+            self.repeat_last_search(cursor, reverse=False)
+            return True
+
+        if key_sequence == ',':
+            self.repeat_last_search(cursor, reverse=True)
+            return True
+        
+        if key_sequence == 'n':
+            print(self.last_word_under_cursor)
             return True
 
         return False
