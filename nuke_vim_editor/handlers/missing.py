beecd7e5a1aeb8c8991e3dc41b798909a8157504
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-07 22:24:17 -0500

    Remove unnecessary keys from missing handler

--- nuke_vim_editor/handlers/missing.py
+++ nuke_vim_editor/handlers/missing.py
@@ -13,8 +13,7 @@ class MissingHandler(BaseHandler):
     def handle(self, params: HandlerParams):
 
         missing = [
-            'p', 'P', 'u', 'U', 'R', '<<', '>>', 'W', 'E', 'B', '.', 'zz',
-            'J'
+            'R', '<<', '>>', 'W', 'E', 'B', '.', 'zz', 'J'
         ]
 
         key_sequence = params.keys
