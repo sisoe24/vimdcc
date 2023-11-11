90424cea80001c9b45ec981b24368e2a4c3f0672
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-11 14:34:32 -0500

    Refactor registers module to use numbered buffer and add new features

--- nuke_vim_editor/registers.py
+++ nuke_vim_editor/registers.py
@@ -1,95 +1,130 @@
 import json
-from typing import Any, Dict, Literal, TypedDict
-
-from PySide2.QtGui import QClipboard
+from typing import (Any, Dict, List, Generic, Literal, TypeVar, Optional,
+                    TypedDict)
 
 
 def increment(index: int) -> str:
     return chr(ord('a') + index - 9) if index >= 9 else str(index + 1)
 
 
-class __Registers:
-    _registers: Dict[str, str] = {}
-    _register_index = 0
+class _NumberedBuffer:
+    def __init__(self, previous_history: list[str], size: int = 10):
+        self.history: list[str] = previous_history or []
+        self.size = size
 
-    @classmethod
-    def get(cls, name: str):
-        return cls._registers.get(name, '')
+    def add(self, item: str):
+        # don't add the same item twice
+        if self.history and item == self.history[0]:
+            return
 
-    @classmethod
-    def get_all(cls):
-        return cls._registers.copy()
+        self.history.insert(0, item)
+        self.history = self.history[: self.size]
 
-    @classmethod
-    def clear(cls):
-        cls._registers.clear()
-        cls._register_index = 0
+    def get(self, index: int):
+        return self.history[index] if 0 <= index < len(self.history) else None
 
-    @classmethod
-    def add(cls, text: str):
-        z_index = 34
-        if cls._register_index >= z_index:
-            cls._register_index = 0
+    def __repr__(self):
+        return str(self.history)
 
-        if cls._registers.get(increment(cls._register_index - 1)) != text:
-            cls._registers[increment(cls._register_index)] = text
-            cls._register_index += 1
 
-        QClipboard().setText(text, QClipboard.Clipboard)
+class Mark(TypedDict):
+    position: int
+    line: str
 
 
 class RegistersTypes(TypedDict):
     named: Dict[str, str]
-    numbered: Dict[str, str]
-    last_search: Dict[str, str]
-    marks: Dict[str, int]
+    numbered: List[str]
+    last_search: str
+    marks: Dict[str, Mark]
 
 
-RegisterName = Literal['named', 'numbered', 'last_search', 'marks']
+RegisterName = Literal['named', 'last_search', 'marks']
+
+T = TypeVar('T', bound=RegisterName)
 
 
-class Registers:
-    _registers: RegistersTypes = {
+class _Registers:
+    registers: RegistersTypes = {
         'named': {},
-        'numbered': {},
-        'last_search': {},
+        'numbered': [],
+        'last_search': '',
         'marks': {},
     }
 
-    def __new__(cls, *args: Any, **kwargs: Any):
-        if not hasattr(cls, '_instance'):
-            cls._instance = super(Registers, cls).__new__(
-                cls, *args, **kwargs)
-        return cls._instance
-
     def __init__(self):
-        # TODO: Expose this as a setting
-        if 'persistent_registers':
-            self._load()
-        else:
-            self.clear()
+        self._load()
+        self._named_buffer = _NumberedBuffer(self.registers['numbered'])
+        self._named_register: Optional[str] = None
 
     def _load(self):
         with open('registers.json') as f:
             data = json.load(f)
             if data:
-                self._registers = data
+                self.registers = data
 
     def _save(self):
         with open('registers.json', 'w') as f:
-            json.dump(self._registers, f)
+            json.dump(self.registers, f, indent=4)
 
-    def update(self, name: RegisterName, key: str, value: Any) -> None:
-        self._registers[name][key] = value
+    def _push_to_clipboard(self, value: str):
+        self._named_buffer.add(value)
+        self.registers['numbered'] = self._named_buffer.history
         self._save()
 
-    def get(self, name: RegisterName, key: str) -> Any:
-        return self._registers[name].get(key, '')
+    def add_mark(self, key: str, text: str, pos: int) -> None:
+        self.registers['marks'][key] = {'position': pos, 'line': text}
+        self._save()
 
-    def get_register(self, name: RegisterName) -> Dict[str, Any]:
-        return self._registers[name]
+    def get_mark(self, key: str) -> Optional[Mark]:
+        return self.registers['marks'].get(key)
+
+    def add_last_search(self, value: str) -> None:
+        self.registers['last_search'] = value
+        self._push_to_clipboard(value)
+
+    def set_named_register(self, key: str) -> None:
+        print('➡ key :', key)
+        self._named_register = key
+
+    def get_numbered_register_value(self, index: int) -> Optional[str]:
+        return self.registers['numbered'][index]
+
+    def get_named_register_value(self) -> Optional[str]:
+        key = self._named_register
+        print('➡ key :', key)
+
+        if not key:
+            self.registers['numbered'][0]
+            return
+
+        if key == '/':
+            return self.registers['last_search']
+
+        return self.registers['named'].get(key)
+
+    def push(self, value: str) -> None:
+        if self._named_register:
+            self.registers['named'][self._named_register] = value
+
+        self._push_to_clipboard(value)
+        self._named_register = None
+
+    def get_register(self, name: RegisterName) -> Any:
+        return self.registers[name]
 
     def clear(self):
-        for register in self._registers.values():
+        for register in self.registers.values():
             register.clear()
         self._save()
+
+    def __getitem__(self, name: RegisterName) -> T:
+        return self.registers[name]
+
+    def __setitem__(self) -> None:
+        raise ValueError(
+            'Cannot set a register directly. Use Registers.update() instead.'
+        )
+
+
+Registers = _Registers()
