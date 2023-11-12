5da32d0dec0f4333d6c0a10fd88a301f7488d4f4
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-12 16:43:05 -0500

    Add tests for document commands

--- tests/test_document_commands.py
+++ tests/test_document_commands.py
@@ -44,7 +44,7 @@ def test_move_document_no_selection(handler: DocumentHandler, data: MotionTest) 
 
     params = HandlerParams(
         cursor=editor.textCursor(),
-        keys=data.motion,
+        keys='',
         modifiers=[],
         event=None,
         mode=Modes.NORMAL
@@ -55,7 +55,7 @@ def test_move_document_no_selection(handler: DocumentHandler, data: MotionTest) 
     for motion in data.motion:
         params.keys = motion
         handler.handle(params)
-    editor.setTextCursor(params.cursor)
+        editor.setTextCursor(params.cursor)
 
     cursor = editor.textCursor()
     position = cursor.position()
@@ -65,3 +65,128 @@ def test_move_document_no_selection(handler: DocumentHandler, data: MotionTest) 
     assert cursor.anchor() == data.expected_pos
     assert cursor.hasSelection() is False
     assert not cursor.selectedText()
+
+
+# paragraph separator
+P = '\u2029'
+
+
+@pytest.mark.parametrize('data', [
+    MotionTest(['gg'], 'foo\nbar\nfoo\nbar', 7, f'foo{P}bar', 0),
+    MotionTest(['G'], 'foo\nbar', 0, f'foo{P}bar', 14),
+    MotionTest(['}'], 'foo\n\nbar\n\nfoo\n\nbar', 0, f'foo{P}', 4),
+    MotionTest(['}', '}'], 'foo\n\nbar\n\nfoo\n\nbar', 0, f'foo{P}{P}bar{P}', 9),
+    MotionTest(['{', '{'], 'foo\n\nbar\n\nfoo\n\nbar', 8, f'foo{P}{P}bar', 4),
+])
+def test_move_document_selection(handler: DocumentHandler, data: MotionTest) -> None:
+    editor = handler.editor
+    editor.setPlainText(data.text)
+
+    params = HandlerParams(
+        cursor=editor.textCursor(),
+        keys='',
+        modifiers=[],
+        event=None,
+        mode=Modes.VISUAL
+    )
+
+    params.cursor.setPosition(data.cursor_start)
+
+    for motion in data.motion:
+        params.keys = motion
+        handler.handle(params)
+        editor.setTextCursor(params.cursor)
+
+    cursor = editor.textCursor()
+
+    assert cursor.hasSelection() is True
+    assert cursor.selectedText() == data.expected_text
+
+
+@pytest.mark.parametrize('data', [
+    MotionTest(['c', 'gg'], 'foo\nbar', 7, '', 0),
+    MotionTest(['c', 'G'], 'foo\nbar', 0, '', 0),
+    MotionTest(['c', '}'], 'foo\n\nbar', 0, '\nbar', 0),
+    MotionTest(['c', '}', '}'], 'foo\n\nbar', 0, '', 0),
+    MotionTest(['c', '{', ], 'foo\n\nbar', 8, 'foo\n', 0),
+    MotionTest(['c', '{', '{'], 'foo\n\nbar', 8, '', 0),
+])
+def test_move_document_edit(handler: DocumentHandler, data: MotionTest) -> None:
+    editor = handler.editor
+    editor.setPlainText(data.text)
+
+    params = HandlerParams(
+        cursor=editor.textCursor(),
+        keys='',
+        modifiers=[],
+        event=None,
+        mode=Modes.CHANGE
+    )
+
+    params.cursor.setPosition(data.cursor_start)
+
+    for motion in data.motion:
+        params.keys = motion
+        handler.handle(params)
+        editor.setTextCursor(params.cursor)
+
+    assert editor.toPlainText() == data.expected_text
+
+
+@pytest.mark.parametrize('data', [
+    MotionTest(['c', 'gg'], 'foo\nbar', 7, '', 0),
+    MotionTest(['c', 'G'], 'foo\nbar', 0, '', 0),
+    MotionTest(['c', '}'], 'foo\n\nbar', 0, '\nbar', 0),
+    MotionTest(['c', '}', '}'], 'foo\n\nbar', 0, '', 0),
+    MotionTest(['c', '{', ], 'foo\n\nbar', 8, 'foo\n', 0),
+    MotionTest(['c', '{', '{'], 'foo\n\nbar', 8, '', 0),
+])
+def test_move_document_delete(handler: DocumentHandler, data: MotionTest) -> None:
+    editor = handler.editor
+    editor.setPlainText(data.text)
+
+    params = HandlerParams(
+        cursor=editor.textCursor(),
+        keys='',
+        modifiers=[],
+        event=None,
+        mode=Modes.DELETE
+    )
+
+    params.cursor.setPosition(data.cursor_start)
+
+    for motion in data.motion:
+        params.keys = motion
+        handler.handle(params)
+        editor.setTextCursor(params.cursor)
+
+    assert editor.toPlainText() == data.expected_text
+
+
+@pytest.mark.parametrize('data', [
+    MotionTest(['gg'], 'foo\nbar\nfoo\nbar', 7, f'foo{P}bar', 0),
+    MotionTest(['G'], 'foo\nbar', 0, f'foo{P}bar', 14),
+    MotionTest(['}'], 'foo\n\nbar\n\nfoo\n\nbar', 0, f'foo{P}', 4),
+    MotionTest(['{'], 'foo\n\nbar\n\nfoo\n\nbar', 8, f'{P}bar', 4),
+])
+def test_move_document_yank(handler: DocumentHandler, data: MotionTest) -> None:
+    editor = handler.editor
+    editor.setPlainText(data.text)
+
+    params = HandlerParams(
+        cursor=editor.textCursor(),
+        keys='',
+        modifiers=[],
+        event=None,
+        mode=Modes.YANK
+    )
+
+    params.cursor.setPosition(data.cursor_start)
+
+    for motion in data.motion:
+        params.keys = motion
+        handler.handle(params)
+        editor.setTextCursor(params.cursor)
+
+    register = handler.registers.get_register('numbered')
+    assert register[0] == data.expected_text
