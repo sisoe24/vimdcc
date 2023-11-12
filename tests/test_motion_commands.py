ca6e4a3d05bca1714be17e62c9935cd0d5175b29
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-12 14:47:45 -0500

    Refactor motion command tests

--- tests/test_motion_commands.py
+++ tests/test_motion_commands.py
@@ -63,7 +63,7 @@ def test_motion_no_select(
 
     params = HandlerParams(
         cursor=editor.textCursor(),
-        keys=data.motion,
+        keys='',
         modifiers=[],
         event=None,
         mode=Modes.NORMAL
@@ -109,16 +109,13 @@ def test_motion_no_select(
     MotionTest(['j'], 'foo\nbar', 0, 'foo\u2029', 4),
     MotionTest(['j'], 'foo\nbar', 2, 'o\u2029ba', 6),
 ])
-def test_motion_select(
-    handler: MotionHandler,
-    data: MotionTest
-):
+def test_motion_select(handler: MotionHandler, data: MotionTest):
     editor = handler.editor
     editor.setPlainText(data.text)
 
     params = HandlerParams(
         cursor=editor.textCursor(),
-        keys=data.motion,
+        keys='',
         modifiers=[],
         event=None,
         mode=Modes.VISUAL
@@ -136,3 +133,126 @@ def test_motion_select(
     assert cursor.anchor() == data.cursor_start
     assert cursor.hasSelection() is True
     assert cursor.selectedText() == data.expected_text
+
+
+@pytest.mark.parametrize('data', [
+    MotionTest(['y', 'w'], 'foo bar', 0, 'foo ', 0),
+    MotionTest(['y', 'e'], 'foo bar', 0, 'foo', 0),
+    MotionTest(['y', 'b'], 'foo bar', 7, 'bar', 7),
+    MotionTest(['y', '$'], 'foo bar', 0, 'foo bar', 0),
+    MotionTest(['y', '0'], '  foo bar', 9, '  foo bar', 9),
+    MotionTest(['y', '^'], '  foo bar', 9, 'foo bar', 9),
+    MotionTest(['y', 'l'], 'foo bar', 0, 'f', 0),
+    MotionTest(['y', 'h'], 'foo bar', 1, 'f', 1),
+    MotionTest(['y', 'j'], 'foo\nbar', 0, 'foo\u2029bar', 0),
+    MotionTest(['y', 'j'], 'foo\nbar', 3, 'foo\u2029bar', 3),
+    MotionTest(['y', 'k'], 'foo\nbar', 4, 'foo\u2029bar', 4),
+    MotionTest(['y', 'k'], 'foo\nbar', 7, 'foo\u2029bar', 7),
+])
+def test_motion_yank(handler: MotionHandler, data: MotionTest):
+    editor = handler.editor
+    editor.setPlainText(data.text)
+
+    params = HandlerParams(
+        cursor=editor.textCursor(),
+        keys='',
+        modifiers=[],
+        event=None,
+        mode=Modes.YANK
+    )
+
+    params.cursor.setPosition(data.cursor_start)
+
+    for motion in data.motion:
+        params.keys = motion
+        handler.handle(params)
+        editor.setTextCursor(params.cursor)
+
+    cursor = editor.textCursor()
+
+    assert cursor.position() == data.expected_pos
+
+    register = handler.registers.get_register('numbered')
+    assert register[0] == data.expected_text
+
+
+@pytest.mark.parametrize('data', [
+    MotionTest(['c', 'w'], 'foo bar', 0, 'bar', 0),
+    MotionTest(['c', 'e'], 'foo bar', 0, ' bar', 0),
+    MotionTest(['c', 'b'], 'foo bar', 7, 'foo ', 4),
+    MotionTest(['c', '$'], 'foo bar', 0, '', 0),
+    MotionTest(['c', '0'], '  foo bar', 9, '', 0),
+    MotionTest(['c', '^'], '  foo bar', 9, '  ', 2),
+    MotionTest(['c', 'l'], 'foo bar', 0, 'oo bar', 0),
+    MotionTest(['c', 'h'], 'foo bar', 1, 'oo bar', 0),
+    MotionTest(['c', 'j'], 'foo\nbar', 0, '', 0),
+    MotionTest(['c', 'j'], 'foo\nbar', 3, '', 0),
+    MotionTest(['c', 'k'], 'foo\nbar', 4, '', 0),
+    MotionTest(['c', 'k'], 'foo\nbar', 7, '', 0),
+])
+def test_motion_edit_change(handler: MotionHandler, data: MotionTest):
+    editor = handler.editor
+    editor.setPlainText(data.text)
+
+    params = HandlerParams(
+        cursor=editor.textCursor(),
+        keys='',
+        modifiers=[],
+        event=None,
+        mode=Modes.CHANGE
+    )
+
+    params.cursor.setPosition(data.cursor_start)
+
+    for motion in data.motion:
+        params.keys = motion
+        handler.handle(params)
+        editor.setTextCursor(params.cursor)
+
+    cursor = editor.textCursor()
+
+    assert cursor.position() == data.expected_pos
+    assert editor.toPlainText() == data.expected_text
+
+    # TODO: Should test if the mode is changed to INSERT but it doesn't work
+    # if I call the handler since its not it who changes the mode
+    # assert handler.mode == Modes.INSERT
+
+
+@pytest.mark.parametrize('data', [
+    MotionTest(['d', 'w'], 'foo bar', 0, 'bar', 0),
+    MotionTest(['d', 'e'], 'foo bar', 0, ' bar', 0),
+    MotionTest(['d', 'b'], 'foo bar', 7, 'foo ', 4),
+    MotionTest(['d', '$'], 'foo bar', 0, '', 0),
+    MotionTest(['d', '0'], '  foo bar', 9, '', 0),
+    MotionTest(['d', '^'], '  foo bar', 9, '  ', 2),
+    MotionTest(['d', 'l'], 'foo bar', 0, 'oo bar', 0),
+    MotionTest(['d', 'h'], 'foo bar', 1, 'oo bar', 0),
+    MotionTest(['d', 'j'], 'foo\nbar', 0, '', 0),
+    MotionTest(['d', 'j'], 'foo\nbar', 3, '', 0),
+    MotionTest(['d', 'k'], 'foo\nbar', 4, '', 0),
+    MotionTest(['d', 'k'], 'foo\nbar', 7, '', 0),
+])
+def test_motion_edit_delete(handler: MotionHandler, data: MotionTest):
+    editor = handler.editor
+    editor.setPlainText(data.text)
+
+    params = HandlerParams(
+        cursor=editor.textCursor(),
+        keys='',
+        modifiers=[],
+        event=None,
+        mode=Modes.CHANGE
+    )
+
+    params.cursor.setPosition(data.cursor_start)
+
+    for motion in data.motion:
+        params.keys = motion
+        handler.handle(params)
+        editor.setTextCursor(params.cursor)
+
+    cursor = editor.textCursor()
+
+    assert cursor.position() == data.expected_pos
+    assert editor.toPlainText() == data.expected_text
