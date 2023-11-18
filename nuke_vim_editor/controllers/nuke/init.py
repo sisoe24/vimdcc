aedfcb2b6a71c7095dff04ee4721c629ca7cd0ae
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-18 18:00:38 -0500

    Add Nuke Vim Editor Lite plugin

---
+++ nuke_vim_editor/controllers/nuke/init.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import pathlib
+
+print('''\
+Nuke Vim Editor Lite.
+Version: 0.1.0
+''')
+
+# Add the plugin to the sys.path
+sys.path.append(str(pathlib.Path(__file__).parent.parent))
