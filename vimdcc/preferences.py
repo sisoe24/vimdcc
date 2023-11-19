f46a12862d2ad351851d301003281936e730a28b
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-19 12:48:45 -0500

    Refactor VimDcc preferences module

--- vimdcc/preferences.py
+++ vimdcc/preferences.py
@@ -1,27 +1,30 @@
-import json
 
-from PySide2.QtWidgets import QLabel, QWidget, QVBoxLayout
+from PySide2.QtCore import Slot
+from PySide2.QtWidgets import (QLabel, QWidget, QCheckBox, QMessageBox,
+                               QVBoxLayout)
+
+from .settings import Settings, _VimDccSettings
 
 
 class VimPreferencesModel:
-    def __init__(self):
-        self._settings = self.load()
+    def __init__(self, settings: _VimDccSettings):
+        self._settings = settings
 
-    def load(self):
-        with open('vimdcc.json') as f:
-            return json.load(f)
+    def set_launch_on_startup(self, value: bool):
+        self._settings.set('launch_on_startup', value)
 
-    def save(self):
-        with open('vimdcc.json', 'w') as f:
-            json.dump(self._settings, f)
+    def launch_on_startup(self) -> bool:
+        return self._settings.get('launch_on_startup', False)
 
 
 class VimPreferencesView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
+        self.launch_on_startup = QCheckBox('Launch on Startup')
+
         layout = QVBoxLayout()
-        layout.addWidget(QLabel('HELLO'))
+        layout.addWidget(self.launch_on_startup)
         self.setLayout(layout)
 
 
@@ -30,12 +33,28 @@ class VimPreferencesController:
         self._view = view
         self._model = model
 
+        self._view.launch_on_startup.stateChanged.connect(self._on_launch_on_startup)
+
+    @Slot(int)
+    def _on_launch_on_startup(self, state: int):
+        QMessageBox.information(
+            self._view, 'VimDcc', 'You must restart VimDcc for this change to take effect.'
+        )
+        self._model.set_launch_on_startup(state == 2)
+
+    def init(self):
+        widget = self._view.launch_on_startup
+        widget.blockSignals(True)
+        widget.setChecked(self._model.launch_on_startup())
+        widget.blockSignals(False)
+
 
 class VimPreferences:
     def __init__(self):
-        self._model = VimPreferencesModel()
+        self._model = VimPreferencesModel(Settings)
         self._view = VimPreferencesView()
         self._controller = VimPreferencesController(self._view, self._model)
+        self._controller.init()
 
     def view(self):
         return self._view
