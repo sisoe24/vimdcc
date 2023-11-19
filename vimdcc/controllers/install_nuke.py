3de4c78c680b9e6ace89b86bc380787c96883756
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-19 11:41:02 -0500

    Refactor PySide2 imports and update get_script_editor and get_console_editor functions

--- vimdcc/controllers/install_nuke.py
+++ vimdcc/controllers/install_nuke.py
@@ -4,45 +4,51 @@ import pathlib
 import contextlib
 from typing import Optional
 
-from PySide2.QtWidgets import QWidget, QSplitter, QApplication, QPlainTextEdit
+from PySide2.QtWidgets import (QLabel, QWidget, QLineEdit, QSplitter,
+                               QVBoxLayout, QApplication, QPlainTextEdit)
 
 from ..main import VimDCC
 from ..utils import cache
 
 
 @cache
-def get_script_editor() -> Optional[QWidget]:
-    return next(
-        (
-            widget
-            for widget in QApplication.allWidgets()
-            if 'scripteditor.1' in widget.objectName()
-        ),
-        None,
-    )
+def get_script_editor() -> QPlainTextEdit:
+    for widget in QApplication.allWidgets():
+        if 'scripteditor.1' in widget.objectName():
+            return widget
+    raise RuntimeError('Could not find script editor')
 
 
 @cache
-def get_console() -> Optional[QWidget]:
-    script_editor = get_script_editor()
-    return script_editor.findChild(QSplitter) if script_editor else None
+def get_console() -> QSplitter:
+    return get_script_editor().findChild(QSplitter)
 
 
 @cache
-def get_console_editor() -> Optional[QPlainTextEdit]:
-    console = get_console()
-    input_editor = console.findChild(QPlainTextEdit)
-    return input_editor or None
+def get_console_editor() -> QPlainTextEdit:
+    input_editor = get_console().findChild(QPlainTextEdit)
+    if not input_editor:
+        raise RuntimeError('Could not find input editor')
+    return input_editor
 
 
 class NukeVimDCC(VimDCC):
     def __init__(self, parent=None):
         super().__init__(get_console_editor(), parent)
 
+        self.console = get_console()
+
+    # def load_status_bar(self):
+    #     if not 'shoudl_load':
+    #         return
+
+    #     self.console.addWidget(self.status_bar)
+    #     self.console.setStretchFactor(0, 1)  # First textEdit takes up maximum space
+    #     self.console.setStretchFactor(1, 1)  # Second textEdit takes up maximum space
+    #     self.console.setStretchFactor(2, 0)  # lineEdit takes up minimum space
+
 
 def install_nuke_vim():
-    # sys.path.append(os.path.dirname(__file__))
-    sys.path.append(str(pathlib.Path(__file__).parent.parent))
     with contextlib.suppress(ImportError):
         import nukescripts
         nukescripts.panels.registerWidgetAsPanel(
