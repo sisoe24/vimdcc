7038b2e2aae39310352ab69f8b9fda32e707a996
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-18 22:16:05 -0500

    Refactor VimDCC widget layout and add clear register button

--- vimdcc/main.py
+++ vimdcc/main.py
@@ -1,19 +1,16 @@
-
 import logging
-import pathlib
 from typing import Any, Dict
-from importlib import import_module
 
 from PySide2.QtCore import Slot
-from PySide2.QtWidgets import (QLabel, QWidget, QPushButton, QVBoxLayout,
-                               QPlainTextEdit)
+from PySide2.QtWidgets import (QLabel, QWidget, QCheckBox, QPushButton,
+                               QVBoxLayout, QPlainTextEdit)
 
-from .status_bar import status_bar
+# TODO: Figure out a cleaner way to do this
+# Dont' remove these imports, they are needed for the cache decorator
+from .handlers import normal, missing
+from .status_bar import _StatusBar, status_bar
 from .utils.cache import cache
 from .editor_modes import EDITOR_MODES
-
-for module in pathlib.Path(__file__).parent.glob('handlers/*.py'):
-    import_module(f'vimdcc.handlers.{module.stem}')
 
 LOGGER = logging.getLogger('vim')
 _EVENT_FILTERS: Dict[str, Any] = {}
@@ -29,13 +26,26 @@ class VimDCC(QWidget):
         self.toggle_vim.clicked.connect(self._on_toggle_vim)
         self.toggle_vim.setCheckable(True)
 
+        self.clear_register = QPushButton('Clear Registers')
+        self.clear_register.clicked.connect(self._on_clear_register)
+
+        self.launch_on_startup = QCheckBox('Launch on Startup')
+
         self.vim_status = QLabel('Vim: OFF')
 
         layout = QVBoxLayout()
-        layout.addWidget(QLabel('<h1>VimLite</h1>'))
+        layout.addWidget(QLabel('<h1>VimDcc</h1>'))
         layout.addWidget(self.vim_status)
         layout.addWidget(self.toggle_vim)
+        layout.addWidget(self.clear_register)
+        layout.addWidget(self.launch_on_startup)
+        layout.addWidget(_StatusBar())
+        layout.addStretch()
         self.setLayout(layout)
+
+    @Slot()
+    def _on_clear_register(self):
+        LOGGER.debug('Clearing register')
 
     @Slot(bool)
     def _on_toggle_vim(self, checked: bool):
@@ -49,6 +59,7 @@ class VimDCC(QWidget):
             LOGGER.debug('Turning Vim OFF')
             self.vim_status.setText('Vim: OFF')
             action = self.editor.removeEventFilter
+            self.editor.setCursorWidth(2)
 
         for mode in EDITOR_MODES:
             LOGGER.debug(f'Installing: {mode.__name__}')
