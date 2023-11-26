607e093c3fc3d4d7614c8fdb844a16d1652d8a01
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-11-26 17:25:25 -0500

    Refactor LocalVimDcc class and update LocalWidget class

--- vimdcc/controllers/local.py
+++ vimdcc/controllers/local.py
@@ -18,19 +18,30 @@ def read_sample(debug=False):
         return f.read()
 
 
-class LocalVimDcc(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
+class LocalVimDcc(VimDCC):
+    def __init__(self):
+        # create the editor before calling super().__init__()
+        # otherwise the editor will not be available in the super class
+        # if the launch_on_startup is True
         self.editor = QPlainTextEdit()
         self.editor.setPlainText(read_sample())
 
-        self.vim_dcc = VimDCC(self.editor)
+        super().__init__()
+
+    def get_editor(self) -> QPlainTextEdit:
+        return self.editor
+
+
+class LocalWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.vim_dcc = LocalVimDcc()
 
         layout = QVBoxLayout()
         layout.addWidget(self.vim_dcc)
         layout.addWidget(QLabel('<h1>Local VimDcc</h1>'))
-        layout.addWidget(self.editor)
+        layout.addWidget(self.vim_dcc.editor)
         layout.addWidget(self.vim_dcc.status_bar)
         self.setLayout(layout)
 
@@ -45,9 +56,9 @@ def main():
 
     set_theme(app)
 
-    window = LocalVimDcc()
+    window = LocalWidget()
     window.setGeometry(100, 100, 1000, 1000)
-    window.editor.setFocus()
+    window.vim_dcc.editor.setFocus()
 
     window.show()
     app.exec_()
